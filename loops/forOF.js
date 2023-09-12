551ae44531d56528a1931ad931fb35b066f7628b
mszhdgr <smwavali876@gmail.com>
2023-09-12 17:57:03 +0300

    using for of to print letters

---
+++ loops/forOF.js
@@ -0,0 +1,11 @@
+// we discuss for of - used in arrays
+//
+function printLetters()
+{
+	let letters = ['a', 'b', 'c', 'd', 'e']
+
+	for (const i of letters){
+		console.log(i)
+	}
+}
+printLetters()
